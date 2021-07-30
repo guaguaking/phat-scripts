9d736937b396d2626a4298f8fc58248ba48a06bd
wangyx <wangyx@clouddream.net>
2021-07-30 10:20:14 +0800

    修改.npmrc文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,47 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  "root": true,
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    // "plugin:@typescript-eslint/recommended"
+  ],
+  // "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        // "plugin:@typescript-eslint/recommended"
-    ],
-    // "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        // "@typescript-eslint"
-    ],
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    },
-    "rules": {
-        "no-var": [
-            "error"
-        ],
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "semi": [
-            "warn",
-            "always"
-        ]
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    // "@typescript-eslint"
+  ],
+  "settings": {
+    "react": {
+      "version": "detect"
     }
-}
+  },
+  "rules": {
+    "no-var": [
+      "error"
+    ],
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "semi": [
+      "warn",
+      "always"
+    ]
+  }
+};
